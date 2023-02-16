cee17d0d9ff74b6f9a2371064e128d505140dde0
ad-freegames <103201397+ad-freegames@users.noreply.github.com>
2023-02-16 11:43:23 +0700

    Update random.js

--- js/random.js
+++ js/random.js
@@ -8,13 +8,13 @@ var images=new Array(totallinks)
 var titles=new Array(totallinks)
 
 urls[0]="https://coolgames24h.blogspot.com/2022/03/subway-surfers-world-tour-beijing.html"
-images[0]="https://1.bp.blogspot.com/-TM0QNc_hhqw/YTm0gRzZm7I/AAAAAAAAADk/TRGIfexgIjoeMKYdwX5D9yGe6uu9lmVOACLcBGAsYHQ/w300-h233-c/subway-surfers-beijing.jpg"
+images[0]="https://1.bp.blogspot.com/-TM0QNc_hhqw/YTm0gRzZm7I/AAAAAAAAADk/TRGIfexgIjoeMKYdwX5D9yGe6uu9lmVOACLcBGAsYHQ/w233-h233-c/subway-surfers-beijing.jpg"
 
 urls[1]="https://coolgames24h.blogspot.com/2022/03/subway-surfers-world-tour-zurich.html"
-images[1]="https://1.bp.blogspot.com/-sWFAatMkXbw/YTmnucsLLXI/AAAAAAAAADM/54ADOwJJX-8K8dFo8HelRVKB7ZVCVbYVQCLcBGAsYHQ/w300-h233-c/subway-surfers-zurich.jpg"
+images[1]="https://1.bp.blogspot.com/-sWFAatMkXbw/YTmnucsLLXI/AAAAAAAAADM/54ADOwJJX-8K8dFo8HelRVKB7ZVCVbYVQCLcBGAsYHQ/w233-h233-c/subway-surfers-zurich.jpg"
 
 urls[2]="https://coolgames24h.blogspot.com/2022/03/subway-surfers-world-tour-san-francisco.html"
-images[2]="https://1.bp.blogspot.com/-Vkwv6DHwW9Y/YTmx1UhFftI/AAAAAAAAADc/8ZiiDWgHix8g7E5ojIPGRMhOausTQtAqACLcBGAsYHQ/w300-h233-c/subway-surfers-san-francisco.jpg"
+images[2]="https://1.bp.blogspot.com/-Vkwv6DHwW9Y/YTmx1UhFftI/AAAAAAAAADc/8ZiiDWgHix8g7E5ojIPGRMhOausTQtAqACLcBGAsYHQ/w233-h233-c/subway-surfers-san-francisco.jpg"
 
 var randomgame=Math.floor(Math.random()*totallinks)
 var randomgame2=Math.floor(Math.random()*totallinks)
