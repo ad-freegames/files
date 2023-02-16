077ffc6ccb86f57923fab2da72f021ac2a87d9af
ad-freegames <103201397+ad-freegames@users.noreply.github.com>
2023-02-16 07:16:15 +0700

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,265 +1,265 @@
 //** Text ad script by Petalia.org **//
 
 //specify total # of random links
-var totallinks=33
+var totallinks=84
 
 var urls=new Array(totallinks)
 var images=new Array(totallinks)
 var titles=new Array(totallinks)
 
-urls[0]="/"
+urls[0]="https://coolgames24h.blogspot.com/"
 images[0]="https://tpc.googlesyndication.com/simgad/2214601101586390609"
 
-urls[1]="/"
+urls[1]="https://coolgames24h.blogspot.com/"
 images[1]="https://tpc.googlesyndication.com/simgad/7340780122315886153"
 
-urls[2]="/"
+urls[2]="https://coolgames24h.blogspot.com/"
 images[2]="https://tpc.googlesyndication.com/simgad/7700200904108616184"
 
-urls[3]="/"
+urls[3]="https://coolgames24h.blogspot.com/"
 images[3]="https://tpc.googlesyndication.com/simgad/18012713050088649335"
 
-urls[4]="/"
+urls[4]="https://coolgames24h.blogspot.com/"
 images[4]="https://tpc.googlesyndication.com/simgad/5064231974830408315"
 
-urls[5]="/"
+urls[5]="https://coolgames24h.blogspot.com/"
 images[5]="https://tpc.googlesyndication.com/simgad/9672358934950132986"
 
-urls[6]="/"
+urls[6]="https://coolgames24h.blogspot.com/"
 images[6]="https://tpc.googlesyndication.com/simgad/10500307277608340622"
 
-urls[7]="/"
+urls[7]="https://coolgames24h.blogspot.com/"
 images[7]="https://tpc.googlesyndication.com/simgad/851434294181687065"
 
-urls[8]="/"
+urls[8]="https://coolgames24h.blogspot.com/"
 images[8]="https://tpc.googlesyndication.com/simgad/4823819359870532413"
 
-urls[9]="/"
+urls[9]="https://coolgames24h.blogspot.com/"
 images[9]="https://tpc.googlesyndication.com/simgad/14843862509473902967"
 
-urls[10]="/"
+urls[10]="https://coolgames24h.blogspot.com/"
 images[10]="https://tpc.googlesyndication.com/simgad/4743205374151822706"
 
-urls[11]="/"
+urls[11]="https://coolgames24h.blogspot.com/"
 images[11]="https://tpc.googlesyndication.com/simgad/16513342181830105209"
 
-urls[12]="/"
+urls[12]="https://coolgames24h.blogspot.com/"
 images[12]="https://tpc.googlesyndication.com/simgad/10283013661945400621"
 
-urls[13]="/"
+urls[13]="https://coolgames24h.blogspot.com/"
 images[13]="https://tpc.googlesyndication.com/simgad/18304369947012416662"
 
-urls[14]="/"
+urls[14]="https://coolgames24h.blogspot.com/"
 images[14]="https://tpc.googlesyndication.com/simgad/10166149925152129205"
 
-urls[15]="/"
+urls[15]="https://coolgames24h.blogspot.com/"
 images[15]="https://tpc.googlesyndication.com/simgad/11613236893504494371"
 
-urls[16]="/"
+urls[16]="https://coolgames24h.blogspot.com/"
 images[16]="https://tpc.googlesyndication.com/simgad/1496238822238239006"
 
-urls[17]="/"
+urls[17]="https://coolgames24h.blogspot.com/"
 images[17]="https://tpc.googlesyndication.com/simgad/2032728609773414249"
 
-urls[18]="/"
+urls[18]="https://coolgames24h.blogspot.com/"
 images[18]="https://tpc.googlesyndication.com/simgad/3628861555095186728"
 
-urls[19]="/"
+urls[19]="https://coolgames24h.blogspot.com/"
 images[19]="https://tpc.googlesyndication.com/simgad/8443528021250698062"
 
-urls[20]="/"
+urls[20]="https://coolgames24h.blogspot.com/"
 images[20]="https://tpc.googlesyndication.com/simgad/1867532978252216016"
 
-urls[21]="/"
+urls[21]="https://coolgames24h.blogspot.com/"
 images[21]="https://tpc.googlesyndication.com/simgad/712690186880230744"
 
-urls[22]="/"
+urls[22]="https://coolgames24h.blogspot.com/"
 images[22]="https://tpc.googlesyndication.com/simgad/5638748184161543394"
 
-urls[23]="/"
+urls[23]="https://coolgames24h.blogspot.com/"
 images[23]="https://tpc.googlesyndication.com/simgad/11749687394506140527"
 
-urls[24]="/"
+urls[24]="https://coolgames24h.blogspot.com/"
 images[24]="https://tpc.googlesyndication.com/simgad/9740957574061538104"
 
-urls[25]="/"
+urls[25]="https://coolgames24h.blogspot.com/"
 images[25]="https://tpc.googlesyndication.com/simgad/5775167436311221226"
 
-urls[26]="/"
+urls[26]="https://coolgames24h.blogspot.com/"
 images[26]="https://tpc.googlesyndication.com/simgad/4132923389753912000"
 
-urls[27]="/"
+urls[27]="https://coolgames24h.blogspot.com/"
 images[27]="https://tpc.googlesyndication.com/simgad/4057615342600174640"
 
-urls[28]="/"
+urls[28]="https://coolgames24h.blogspot.com/"
 images[28]="https://tpc.googlesyndication.com/simgad/6312465547214811803"
 
-urls[29]="/"
+urls[29]="https://coolgames24h.blogspot.com/"
 images[29]="https://tpc.googlesyndication.com/simgad/6836182721555531974"
 
-urls[30]="/"
+urls[30]="https://coolgames24h.blogspot.com/"
 images[30]="https://tpc.googlesyndication.com/simgad/8889753911707045472"
 
-urls[31]="/"
+urls[31]="https://coolgames24h.blogspot.com/"
 images[31]="https://tpc.googlesyndication.com/simgad/3061341218860275910"
 
-urls[32]="/"
+urls[32]="https://coolgames24h.blogspot.com/"
 images[32]="https://tpc.googlesyndication.com/simgad/10992303186439749386"
 
-urls[33]="/"
+urls[33]="https://coolgames24h.blogspot.com/"
 images[33]="https://tpc.googlesyndication.com/simgad/10250429153868935378"
 
-urls[34]="/"
+urls[34]="https://coolgames24h.blogspot.com/"
 images[34]="https://tpc.googlesyndication.com/simgad/10205694461540843398"
 
-urls[35]="/"
+urls[35]="https://coolgames24h.blogspot.com/"
 images[35]="https://tpc.googlesyndication.com/simgad/9955089334185006066"
 
-urls[36]="/"
+urls[36]="https://coolgames24h.blogspot.com/"
 images[36]="https://tpc.googlesyndication.com/simgad/11779260749772438811"
 
-urls[37]="/"
+urls[37]="https://coolgames24h.blogspot.com/"
 images[37]="https://tpc.googlesyndication.com/simgad/2353456507719114820"
 
-urls[38]="/"
+urls[38]="https://coolgames24h.blogspot.com/"
 images[38]="https://tpc.googlesyndication.com/simgad/14843024360637308351"
 
-urls[39]="/"
+urls[39]="https://coolgames24h.blogspot.com/"
 images[39]="https://tpc.googlesyndication.com/simgad/553603040505052950"
 
-urls[40]="/"
+urls[40]="https://coolgames24h.blogspot.com/"
 images[40]="https://tpc.googlesyndication.com/simgad/12167932441723189699"
 
-urls[41]="/"
+urls[41]="https://coolgames24h.blogspot.com/"
 images[41]="https://tpc.googlesyndication.com/simgad/4177033149105135800"
 
-urls[42]="/"
+urls[42]="https://coolgames24h.blogspot.com/"
 images[42]="https://tpc.googlesyndication.com/simgad/15210574346880738507"
 
-urls[43]="/"
+urls[43]="https://coolgames24h.blogspot.com/"
 images[43]="https://tpc.googlesyndication.com/simgad/9029110852717929135"
 
-urls[44]="/"
+urls[44]="https://coolgames24h.blogspot.com/"
 images[44]="https://tpc.googlesyndication.com/simgad/9699441934714495564"
 
-urls[45]="/"
+urls[45]="https://coolgames24h.blogspot.com/"
 images[45]="https://tpc.googlesyndication.com/simgad/1288429693823685518"
 
-urls[46]="/"
+urls[46]="https://coolgames24h.blogspot.com/"
 images[46]="https://tpc.googlesyndication.com/simgad/8449982175589010209"
 
-urls[47]="/"
+urls[47]="https://coolgames24h.blogspot.com/"
 images[47]="https://tpc.googlesyndication.com/simgad/17815835638821784639"
 
-urls[48]="/"
+urls[48]="https://coolgames24h.blogspot.com/"
 images[48]="https://tpc.googlesyndication.com/simgad/7248545872050013011"
 
-urls[49]="/"
+urls[49]="https://coolgames24h.blogspot.com/"
 images[49]="https://tpc.googlesyndication.com/simgad/2420213026470347565"
 
-urls[50]="/"
+urls[50]="https://coolgames24h.blogspot.com/"
 images[50]="https://tpc.googlesyndication.com/simgad/5469310556035583221"
 
-urls[51]="/"
+urls[51]="https://coolgames24h.blogspot.com/"
 images[51]="https://tpc.googlesyndication.com/simgad/12261332603037073007"
 
-urls[52]="/"
+urls[52]="https://coolgames24h.blogspot.com/"
 images[52]="https://tpc.googlesyndication.com/simgad/4441049223761726027"
 
-urls[53]="/"
+urls[53]="https://coolgames24h.blogspot.com/"
 images[53]="https://tpc.googlesyndication.com/simgad/1865686813378777901"
 
-urls[54]="/"
+urls[54]="https://coolgames24h.blogspot.com/"
 images[54]="https://tpc.googlesyndication.com/simgad/10926825959868835380"
 
-urls[55]="/"
+urls[55]="https://coolgames24h.blogspot.com/"
 images[55]="https://tpc.googlesyndication.com/simgad/11472013780647231866"
 
-urls[56]="/"
+urls[56]="https://coolgames24h.blogspot.com/"
 images[56]="https://tpc.googlesyndication.com/simgad/12664073170080959613"
 
-urls[57]="/"
+urls[57]="https://coolgames24h.blogspot.com/"
 images[57]="https://tpc.googlesyndication.com/simgad/11579458168692864067"
 
-urls[58]="/"
+urls[58]="https://coolgames24h.blogspot.com/"
 images[58]="https://tpc.googlesyndication.com/simgad/3474769925326752432"
 
-urls[59]="/"
+urls[59]="https://coolgames24h.blogspot.com/"
 images[59]="https://tpc.googlesyndication.com/simgad/14560677029252732910"
 
-urls[60]="/"
+urls[60]="https://coolgames24h.blogspot.com/"
 images[60]="https://tpc.googlesyndication.com/simgad/13790109336053559306"
 
-urls[61]="/"
+urls[61]="https://coolgames24h.blogspot.com/"
 images[61]="https://tpc.googlesyndication.com/simgad/8741884138802079463"
 
-urls[62]="/"
+urls[62]="https://coolgames24h.blogspot.com/"
 images[62]="https://tpc.googlesyndication.com/simgad/12063965145246727073"
 
-urls[63]="/"
+urls[63]="https://coolgames24h.blogspot.com/"
 images[63]="https://tpc.googlesyndication.com/simgad/8623982905841953233"
 
-urls[64]="/"
+urls[64]="https://coolgames24h.blogspot.com/"
 images[64]="https://tpc.googlesyndication.com/simgad/10106948051022751509"
 
-urls[65]="/"
+urls[65]="https://coolgames24h.blogspot.com/"
 images[65]="https://tpc.googlesyndication.com/simgad/14546381304422363970"
 
-urls[66]="/"
+urls[66]="https://coolgames24h.blogspot.com/"
 images[66]="https://tpc.googlesyndication.com/simgad/16831867138346066204"
 
-urls[67]="/"
+urls[67]="https://coolgames24h.blogspot.com/"
 images[67]="https://tpc.googlesyndication.com/simgad/9244517109927583229"
 
-urls[68]="/"
+urls[68]="https://coolgames24h.blogspot.com/"
 images[68]="https://tpc.googlesyndication.com/simgad/6053964863183922946"
 
-urls[69]="/"
+urls[69]="https://coolgames24h.blogspot.com/"
 images[69]="https://tpc.googlesyndication.com/simgad/15525543978460751602"
 
-urls[70]="/"
+urls[70]="https://coolgames24h.blogspot.com/"
 images[70]="https://tpc.googlesyndication.com/simgad/17432832114285704785"
 
-urls[71]="/"
+urls[71]="https://coolgames24h.blogspot.com/"
 images[71]="https://tpc.googlesyndication.com/simgad/6221277448329640598"
 
-urls[72]="/"
+urls[72]="https://coolgames24h.blogspot.com/"
 images[72]="https://tpc.googlesyndication.com/simgad/4422949195779099331"
 
-urls[73]="/"
+urls[73]="https://coolgames24h.blogspot.com/"
 images[73]="https://tpc.googlesyndication.com/simgad/13820135049865910036"
 
-urls[74]="/"
+urls[74]="https://coolgames24h.blogspot.com/"
 images[74]="https://tpc.googlesyndication.com/simgad/6402823732997067745"
 
-urls[75]="/"
+urls[75]="https://coolgames24h.blogspot.com/"
 images[75]="https://tpc.googlesyndication.com/simgad/16660676646719697044"
 
-urls[76]="/"
+urls[76]="https://coolgames24h.blogspot.com/"
 images[76]="https://tpc.googlesyndication.com/simgad/17969542624777654665"
 
-urls[77]="/"
+urls[77]="https://coolgames24h.blogspot.com/"
 images[77]="https://tpc.googlesyndication.com/simgad/15571739352159675136"
 
-urls[78]="/"
+urls[78]="https://coolgames24h.blogspot.com/"
 images[78]="https://tpc.googlesyndication.com/simgad/2771663010253195458"
 
-urls[79]="/"
+urls[79]="https://coolgames24h.blogspot.com/"
 images[79]="https://tpc.googlesyndication.com/simgad/769155983717228218"
 
-urls[80]="/"
+urls[80]="https://coolgames24h.blogspot.com/"
 images[80]="https://tpc.googlesyndication.com/simgad/1821758349726530761"
 
-urls[81]="/"
+urls[81]="https://coolgames24h.blogspot.com/"
 images[81]="https://tpc.googlesyndication.com/simgad/6811693811055589001"
 
-urls[82]="/"
+urls[82]="https://coolgames24h.blogspot.com/"
 images[82]="https://tpc.googlesyndication.com/simgad/3612329525175969529"
 
-urls[83]="/"
+urls[83]="https://coolgames24h.blogspot.com/"
 images[83]="https://tpc.googlesyndication.com/simgad/12357281672151208009"
 
-urls[84]="/"
+urls[84]="https://coolgames24h.blogspot.com/"
 images[84]="https://tpc.googlesyndication.com/simgad/13314565272976068338"
 
 var randomgame=Math.floor(Math.random()*totallinks)
