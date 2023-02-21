896b05892db66c1ff6ebe8905f4423bd3c444f54
ad-freegames <103201397+ad-freegames@users.noreply.github.com>
2023-02-21 14:49:02 +0700

    Create random-3.js

---
+++ js/random-3.js
@@ -0,0 +1,82 @@
+//** Text ad script by Petalia.org **//
+
+//specify total # of random links
+var totallinks=22
+
+var urls=new Array(totallinks)
+var images=new Array(totallinks)
+var titles=new Array(totallinks)
+
+urls[0]="https://coolgames24h.blogspot.com/2022/12/zoom-be.html"
+images[0]="https://tpc.googlesyndication.com/simgad/7700200904108616184"
+
+urls[1]="https://coolgames24h.blogspot.com/2022/12/idle-ants.html"
+images[1]="https://tpc.googlesyndication.com/simgad/851434294181687065"
+
+urls[2]="https://coolgames24h.blogspot.com/2022/12/zoom-be-2.html"
+images[2]="https://tpc.googlesyndication.com/simgad/4823819359870532413"
+
+urls[3]="https://coolgames24h.blogspot.com/2022/12/g-switch-3.html"
+images[3]="https://tpc.googlesyndication.com/simgad/14843862509473902967"
+
+urls[4]="https://coolgames24h.blogspot.com/2023/02/skiing-fred.html"
+images[4]="https://tpc.googlesyndication.com/simgad/4743205374151822706"
+
+urls[5]="https://coolgames24h.blogspot.com/2023/02/temple-run-2.html"
+images[5]="https://tpc.googlesyndication.com/simgad/10166149925152129205"
+
+urls[6]="https://coolgames24h.blogspot.com/2023/01/stickman-climb-2.html"
+images[6]="https://tpc.googlesyndication.com/simgad/1867532978252216016"
+
+urls[7]="https://coolgames24h.blogspot.com/2023/01/subway-surfers-world-tour-marrakesh.html"
+images[7]="https://tpc.googlesyndication.com/simgad/4057615342600174640"
+
+urls[8]="https://coolgames24h.blogspot.com/2022/12/brain-test-2-tricky-stories.html"
+images[8]="https://tpc.googlesyndication.com/simgad/6312465547214811803"
+
+urls[9]="https://coolgames24h.blogspot.com/2023/01/bouncy-basketball.html"
+images[9]="https://tpc.googlesyndication.com/simgad/6836182721555531974"
+
+urls[10]="https://coolgames24h.blogspot.com/2023/02/temple-run-2-frozen-shadows.html"
+images[10]="https://tpc.googlesyndication.com/simgad/3061341218860275910"
+
+urls[11]="https://coolgames24h.blogspot.com/2023/01/who-is.html"
+images[11]="https://tpc.googlesyndication.com/simgad/10250429153868935378"
+
+urls[12]="https://coolgames24h.blogspot.com/2023/01/sugar-sugar.html"
+images[12]="https://tpc.googlesyndication.com/simgad/9955089334185006066"
+
+urls[13]="https://coolgames24h.blogspot.com/2023/02/super-falling-fred.html"
+images[13]="https://tpc.googlesyndication.com/simgad/17815835638821784639"
+
+urls[14]="https://coolgames24h.blogspot.com/2022/12/duo-vikings.html"
+images[14]="https://tpc.googlesyndication.com/simgad/2420213026470347565"
+
+urls[15]="https://coolgames24h.blogspot.com/2022/12/puffy-cat.html"
+images[15]="https://tpc.googlesyndication.com/simgad/11472013780647231866"
+
+urls[16]="https://coolgames24h.blogspot.com/2023/02/raft-wars-multiplayer.html"
+images[16]="https://tpc.googlesyndication.com/simgad/12664073170080959613"
+
+urls[17]="https://coolgames24h.blogspot.com/2022/12/duo-survival-3.html"
+images[17]="https://tpc.googlesyndication.com/simgad/11579458168692864067"
+
+urls[18]="https://coolgames24h.blogspot.com/2022/12/gladihoppers.html"
+images[18]="https://tpc.googlesyndication.com/simgad/8623982905841953233"
+
+urls[19]="https://coolgames24h.blogspot.com/2022/12/zoom-be-3.html"
+images[19]="https://tpc.googlesyndication.com/simgad/9244517109927583229"
+
+urls[20]="https://coolgames24h.blogspot.com/2023/01/like-king.html"
+images[20]="https://tpc.googlesyndication.com/simgad/6053964863183922946"
+
+urls[21]="https://coolgames24h.blogspot.com/2023/01/blumgi-rocket.html"
+images[21]="https://tpc.googlesyndication.com/simgad/17969542624777654665"
+
+urls[22]="https://coolgames24h.blogspot.com/2023/01/backflipper.html"
+images[22]="https://tpc.googlesyndication.com/simgad/3612329525175969529"
+
+var randomgame=Math.floor(Math.random()*totallinks)
+var randomgame2=Math.floor(Math.random()*totallinks)
+var randomgame3=Math.floor(Math.random()*totallinks)
+var randomgame4=Math.floor(Math.random()*totallinks)
