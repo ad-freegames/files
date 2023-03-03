410c48623e9c26ff20d259fc6973f8aa30ad0c23
ad-freegames <103201397+ad-freegames@users.noreply.github.com>
2023-03-04 00:36:33 +0700

    Update random.js

--- js/random.js
+++ js/random.js
@@ -7,73 +7,73 @@ var urls=new Array(totallinks)
 var images=new Array(totallinks)
 var titles=new Array(totallinks)
 
-urls[0]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/zoom-be.html"
+urls[0]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/zoom-be.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[0]="https://tpc.googlesyndication.com/simgad/7700200904108616184"
 
-urls[1]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/idle-ants.html"
+urls[1]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/idle-ants.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[1]="https://tpc.googlesyndication.com/simgad/851434294181687065"
 
-urls[2]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/zoom-be-2.html"
+urls[2]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/zoom-be-2.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[2]="https://tpc.googlesyndication.com/simgad/4823819359870532413"
 
-urls[3]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/g-switch-3.html"
+urls[3]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/g-switch-3.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[3]="https://tpc.googlesyndication.com/simgad/14843862509473902967"
 
-urls[4]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/02/skiing-fred.html"
+urls[4]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/02/skiing-fred.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[4]="https://tpc.googlesyndication.com/simgad/4743205374151822706"
 
-urls[5]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/02/temple-run-2.html"
+urls[5]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/02/temple-run-2.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[5]="https://tpc.googlesyndication.com/simgad/10166149925152129205"
 
-urls[6]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/stickman-climb-2.html"
+urls[6]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/stickman-climb-2.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[6]="https://tpc.googlesyndication.com/simgad/1867532978252216016"
 
-urls[7]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/subway-surfers-marrakesh.html"
+urls[7]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/subway-surfers-marrakesh.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[7]="https://tpc.googlesyndication.com/simgad/4057615342600174640"
 
-urls[8]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/brain-test-2-tricky-stories.html"
+urls[8]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/brain-test-2-tricky-stories.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[8]="https://tpc.googlesyndication.com/simgad/6312465547214811803"
 
-urls[9]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/bouncy-basketball.html"
+urls[9]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/bouncy-basketball.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[9]="https://tpc.googlesyndication.com/simgad/6836182721555531974"
 
-urls[10]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/02/temple-run-2-frozen-shadows.html"
+urls[10]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/02/temple-run-2-frozen-shadows.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[10]="https://tpc.googlesyndication.com/simgad/3061341218860275910"
 
-urls[11]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/who-is.html"
+urls[11]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/who-is.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[11]="https://tpc.googlesyndication.com/simgad/10250429153868935378"
 
-urls[12]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/sugar-sugar.html"
+urls[12]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/sugar-sugar.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[12]="https://tpc.googlesyndication.com/simgad/9955089334185006066"
 
-urls[13]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/02/super-falling-fred.html"
+urls[13]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/02/super-falling-fred.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[13]="https://tpc.googlesyndication.com/simgad/17815835638821784639"
 
-urls[14]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/duo-vikings.html"
+urls[14]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/duo-vikings.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[14]="https://tpc.googlesyndication.com/simgad/2420213026470347565"
 
-urls[15]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/puffy-cat.html"
+urls[15]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/puffy-cat.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[15]="https://tpc.googlesyndication.com/simgad/11472013780647231866"
 
-urls[16]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/02/raft-wars-multiplayer.html"
+urls[16]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/02/raft-wars-multiplayer.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[16]="https://tpc.googlesyndication.com/simgad/12664073170080959613"
 
-urls[17]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/duo-survival-3.html"
+urls[17]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/duo-survival-3.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[17]="https://tpc.googlesyndication.com/simgad/11579458168692864067"
 
-urls[18]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/gladihoppers.html"
+urls[18]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/gladihoppers.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[18]="https://tpc.googlesyndication.com/simgad/8623982905841953233"
 
-urls[19]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/zoom-be-3.html"
+urls[19]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2022/12/zoom-be-3.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[19]="https://tpc.googlesyndication.com/simgad/9244517109927583229"
 
-urls[20]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/like-king.html"
+urls[20]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/like-king.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[20]="https://tpc.googlesyndication.com/simgad/6053964863183922946"
 
-urls[21]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/blumgi-rocket.html"
+urls[21]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/blumgi-rocket.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[21]="https://tpc.googlesyndication.com/simgad/17969542624777654665"
 
-urls[22]="https://googleads.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/backflipper.html"
+urls[22]="https://www.google.com/url?sa=i&url=https%3A%2F%2Funblockedgameszone.blogspot.com/2023/01/backflipper.html&psig=AOvVaw2bislsXhLM19JM4HSv4dJr&ust=1677947892144000"
 images[22]="https://tpc.googlesyndication.com/simgad/3612329525175969529"
 
 var randomgame=Math.floor(Math.random()*totallinks)
